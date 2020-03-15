8ba9291ca3071a46cc7d3bca1f1166818c99d3fe
SYED EBAD <syed.ebad1999@gmail.com>
2020-03-15 17:52:47 +0500

    db url change

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURL : 
-    `mongodb+srv://syedebad:ebad123@cluster0-xt32z.mongodb.net/My_Users?retryWrites=true&w=majority`,
+    `mongodb+srv://syedebad:IXraoFyJXbhWUlYs@cluster0-xt32z.mongodb.net/My_Users?retryWrites=true&w=majority`,
     secretOrKey : 'Secret',
 }
