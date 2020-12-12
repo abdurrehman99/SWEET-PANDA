dcabddecce74bbb5ea07e158e40bd856b990d031
abdurrehman99 <syed.ebad1999@gmail.com>
2020-12-13 01:08:58 +0500

    deploy

--- client/src/components/MyCart.js
+++ client/src/components/MyCart.js
@@ -1,265 +1,298 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import StripeCheckout from 'react-stripe-checkout';
+import React, { Component } from "react";
+import axios from "axios";
+import StripeCheckout from "react-stripe-checkout";
 import { Link } from "react-router-dom";
-import sweetalert from 'sweetalert';
-import { Table } from 'reactstrap';
-import { connect } from 'react-redux';
-import { fillCart } from '../actions/cartAction';
+import sweetalert from "sweetalert";
+import { Table } from "reactstrap";
+import { connect } from "react-redux";
+import { fillCart } from "../actions/cartAction";
 // import PropTypes from  'prop-types';
 
 class MyCart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      address: "",
+      paymentMethod: "",
+      mycart: [],
+      totalAmount: 0,
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            user : {} ,
-            address : '',
-            paymentMethod : '',
-            mycart : [],
-            totalAmount : 0,
-        }
+  componentDidMount = () => {
+    const { mycart } = this.props.cart;
+    console.log(mycart);
+    let { user } = this.props.auth;
+    let totalAmount = 0;
+
+    //Calculate total Bill
+    mycart.forEach((cart, index) => {
+      totalAmount = totalAmount + Number(cart.price) * Number(cart.quantity);
+    });
+
+    // mycart.map(cart=>{
+    //     totalAmount=totalAmount+Number(cart.price)*Number(cart.quantity);
+    // });
+    this.setState({ totalAmount, user, mycart });
+
+    if (this.props.auth.user.email === "admin@sweetpanda.com") {
+      this.props.history.push("/adminPanel");
     }
+  };
 
-    componentDidMount = () =>{
-        
-            const { mycart } = this.props.cart;
-            console.log(mycart);
-            let { user } = this.props.auth;
-            let totalAmount = 0;
+  setAddress = (e) => {
+    this.setState({ address: e.target.value });
+  };
 
-            //Calculate total Bill
-            mycart.forEach( (cart,index)=>{
-                totalAmount=totalAmount+Number(cart.price)*Number(cart.quantity);
-            });
-            
-            // mycart.map(cart=>{
-            //     totalAmount=totalAmount+Number(cart.price)*Number(cart.quantity);
-            // });
-            this.setState({ totalAmount , user , mycart });
-        
-        
-        if(this.props.auth.user.email === 'admin@sweetpanda.com') {
-            this.props.history.push('/adminPanel');
-        }
-        
-    }
+  checkOut = () => {
+    console.log(this.state.mycart);
+    let data = {
+      user: this.state.user,
+      address: this.state.address,
+      paymentMethod: "Cash on Delivery",
+      mycart: this.state.mycart,
+      totalAmount: this.state.totalAmount,
+    };
 
-    setAddress = (e) =>{
-        this.setState({ address : e.target.value });
-    }
-
-    checkOut = () =>{
-        console.log(this.state.mycart);
-        let data = {
-            user : this.state.user,
-            address : this.state.address,
-            paymentMethod : 'Cash on Delivery',
-            mycart : this.state.mycart,
-            totalAmount : this.state.totalAmount,
-        }
-
-        if(this.state.address === ""){
-            sweetalert({
-                icon : 'info',
-                title: "Please enter your address !"
-            });
-        }
-        else{
-            axios.post('/api/orders',data)
-            .then( res=>{
-                sweetalert({
-                    icon : 'success',
-                    title: "Your Order has been placed !"
-                });
-                this.props.history.push("/")
-                this.props.cart.mycart = [];
-                localStorage.removeItem('cart');
-                console.log(res);
-            })
-            .catch( err=>{
-                sweetalert({
-                    icon : 'error',
-                    title: "Error in sending your order !"
-                });
-                console.log(err);
-            });
-        }
-    }
-
-    stripePayment = (token)=>{
-
-        var product = {
-            price: this.state.totalAmount,
-            name : "Sweets"
-        }
-        
-        axios.post('/api/stripe', { token , product })
-            .then( res=> {
-                console.log(res.data.status);
-            })
-            .catch( err=> {
-                console.log(err);
-            });
-
-            let data = {
-                user : this.state.user,
-                address : "Online Payment",
-                paymentMethod : 'Stripe',
-                mycart : this.state.mycart,
-                totalAmount : this.state.totalAmount,
-            }
-            
-        axios.post('/api/orders',data)
-            .then( res=>{
-                sweetalert({
-                    icon : 'success',
-                    title: "Your Order has been placed !"
-                });
-                this.props.history.push("/");
-                this.props.cart.mycart = [];
-                localStorage.removeItem('cart');
-                console.log(res);
-            })
-            .catch( err=>{
-                sweetalert({
-                    icon : 'error',
-                    title: "Error in sending your order !"
-                });
-                console.log(err);
-            });
-    }
-
-    deleteItem = (index)=>{
-        console.log(index);
-        let newCart = [...this.state.mycart];
-        let totalAmount = 0;
-        newCart.splice(index,1);
-
-        //Update local storage of CART
-        localStorage.setItem('cart',JSON.stringify(newCart));
-
-        //Update Redux of CART
-        this.props.fillCart(newCart);
-
-        //Update Amount
-        newCart.forEach( (cart,index)=>{
-            totalAmount=totalAmount+Number(cart.price)*Number(cart.quantity);
-        });
-        
-        // newCart.map( cart=>{
-        //     totalAmount=totalAmount+Number(cart.price)*Number(cart.quantity);
-        // });
-        this.setState({
-            mycart : newCart,
-            totalAmount
+    if (this.state.address === "") {
+      sweetalert({
+        icon: "info",
+        title: "Please enter your address !",
+      });
+    } else {
+      axios
+        .post("/api/orders", data)
+        .then((res) => {
+          sweetalert({
+            icon: "success",
+            title: "Your Order has been placed !",
+          });
+          this.props.history.push("/");
+          this.props.cart.mycart = [];
+          localStorage.removeItem("cart");
+          console.log(res);
+        })
+        .catch((err) => {
+          sweetalert({
+            icon: "error",
+            title: "Error in sending your order !",
+          });
+          console.log(err);
         });
     }
-    render() {
-        if(this.props.auth.isAuthenticated === false){
-            return ( 
-                <div className="container mb-5 pb-5">
-                    <div className="row">
-                        <div className="col-xs-6 col-sm-9">
-                            <h2 className='m-5'>You are not logged in <i className="fas fa-exclamation"></i></h2>
-                        </div>
-                        <div className="col-xs-4 col-sm-3">
-                            <Link to='/login' className='mt-5 btn btn-secondary'><i className="fas fa-sign-in-alt"></i> Login</Link>
-                            <Link to='/itemlist' className='mt-5 btn btn-primary float-right'><i className="fas fa-chevron-left"></i> Go Back</Link>
-                        </div>
-                    </div>
-                </div>
-            )
-        }
-        if(this.state.mycart.length === 0){
-            return ( 
-                <div className="container mb-5 pb-5">
-                    <div className="row">
-                        <div className="col-xs-6 col-sm-8">
-                            <h2 className='m-5'><i className="text-danger fas fa-shopping-cart"></i> Your Cart is Empty !</h2>
-                        </div>
-                        <div className="col-xs-4 col-sm-4">
-                            <Link to='/itemlist' className='m-5 btn btn-primary float-right'><i className="fas fa-chevron-left"></i> Go Back</Link>
-                        </div>
-                    </div>
-                </div>
-            )
-        }
-        else
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-6 col-sm-8">
-                        <h2 className='my-4'>Your Cart <i className="fas fa-shopping-cart"></i> </h2>
-                    </div>
-                    <div className="col-xs-4 col-sm-4">
-                        <Link to='/itemlist' className='my-4 btn btn-primary float-right'><i className="fas fa-chevron-left"></i> Go Back</Link>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-12 col-sm-9">
-                        <Table hover bordered >
-                        <thead>
-                            <tr>
-                                <th>S no.</th>
-                                <th>Item Name</th>
-                                <th>Vendor</th>
-                                <th>Quantity</th>
-                                <th>Price</th>
-                                <th>Amount</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody> 
-                            { this.state.mycart.map( (cart,index)=>{
-                                
-                                return (
-                                    <tr key={index}>
-                                        <td>{index+1}</td>
-                                        <td>{cart.itemName}</td>
-                                        <td>{cart.vendor}</td>
-                                        <td>{cart.quantity}</td>
-                                        <td>{cart.price}</td>
-                                        <td>{Number(cart.price)*Number(cart.quantity)}</td>
-                                        <td className='text-center'><button onClick={ ()=>this.deleteItem(index)} className='btn btn-outline-danger'><i className='fa fa-trash'></i></button></td>
-                                    </tr>
-                                )
-                            })}
-                            <tr>
-                                <td><strong>Total Amount</strong></td>
-                                <td><strong>{this.state.totalAmount} / -</strong></td>
-                            </tr>
-                        </tbody>
-                        </Table>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className="col-xs-12 col-sm-12 col-md-6">
-                        <h4>Cash On Delivery <i className="fas fa-money-bill"></i></h4>
-                        <input onChange={ (e)=>this.setAddress(e) } className='w-75 form-control my-3' placeholder='Enter Your Address*' />
-                        <button onClick={this.checkOut} className='btn btn-outline-success my-1'><i className="fas fa-check-double"></i> Check Out </button>
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-md-6">
-                        <h4 className='mb-3'>Stripe Checkout <i className="fab fa-cc-stripe"></i></h4>
-                        <StripeCheckout 
-                        stripeKey='pk_test_V086ENyvmzdzrDr3hZB3I5hI006Pgfs0TM'
-                        token = {this.stripePayment} 
-                        billingAddress 
-                        amount={this.state.totalAmount} 
-                        name='Sweet Panda' 
-                        email={this.props.auth.user.email} />
-                    </div>
-                </div>
+  };
+
+  stripePayment = (token) => {
+    var product = {
+      price: this.state.totalAmount,
+      name: "Sweets",
+    };
+
+    axios
+      .post("/api/stripe", { token, product })
+      .then((res) => {
+        console.log(res.data.status);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    let data = {
+      user: this.state.user,
+      address: "Online Payment",
+      paymentMethod: "Stripe",
+      mycart: this.state.mycart,
+      totalAmount: this.state.totalAmount,
+    };
+
+    axios
+      .post("/api/orders", data)
+      .then((res) => {
+        sweetalert({
+          icon: "success",
+          title: "Your Order has been placed !",
+        });
+        this.props.history.push("/");
+        this.props.cart.mycart = [];
+        localStorage.removeItem("cart");
+        console.log(res);
+      })
+      .catch((err) => {
+        sweetalert({
+          icon: "error",
+          title: "Error in sending your order !",
+        });
+        console.log(err);
+      });
+  };
+
+  deleteItem = (index) => {
+    console.log(index);
+    let newCart = [...this.state.mycart];
+    let totalAmount = 0;
+    newCart.splice(index, 1);
+
+    //Update local storage of CART
+    localStorage.setItem("cart", JSON.stringify(newCart));
+
+    //Update Redux of CART
+    this.props.fillCart(newCart);
+
+    //Update Amount
+    newCart.forEach((cart, index) => {
+      totalAmount = totalAmount + Number(cart.price) * Number(cart.quantity);
+    });
+
+    // newCart.map( cart=>{
+    //     totalAmount=totalAmount+Number(cart.price)*Number(cart.quantity);
+    // });
+    this.setState({
+      mycart: newCart,
+      totalAmount,
+    });
+  };
+  render() {
+    if (this.props.auth.isAuthenticated === false) {
+      return (
+        <div className="container mb-5 pb-5">
+          <div className="row">
+            <div className="col-xs-6 col-sm-9">
+              <h2 className="m-5">
+                You are not logged in <i className="fas fa-exclamation"></i>
+              </h2>
             </div>
-                
-        )
+            <div className="col-xs-4 col-sm-3">
+              <Link to="/login" className="mt-5 btn btn-secondary">
+                <i className="fas fa-sign-in-alt"></i> Login
+              </Link>
+              <Link to="/itemlist" className="mt-5 btn btn-primary float-right">
+                <i className="fas fa-chevron-left"></i> Go Back
+              </Link>
+            </div>
+          </div>
+        </div>
+      );
     }
+    if (this.state.mycart.length === 0) {
+      return (
+        <div className="container mb-5 pb-5">
+          <div className="row">
+            <div className="col-xs-6 col-sm-8">
+              <h2 className="m-5">
+                <i className="text-danger fas fa-shopping-cart"></i> Your Cart
+                is Empty !
+              </h2>
+            </div>
+            <div className="col-xs-4 col-sm-4">
+              <Link to="/itemlist" className="m-5 btn btn-primary float-right">
+                <i className="fas fa-chevron-left"></i> Go Back
+              </Link>
+            </div>
+          </div>
+        </div>
+      );
+    } else
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-6 col-sm-8">
+              <h2 className="my-4">
+                Your Cart <i className="fas fa-shopping-cart"></i>{" "}
+              </h2>
+            </div>
+            <div className="col-xs-4 col-sm-4">
+              <Link to="/itemlist" className="my-4 btn btn-primary float-right">
+                <i className="fas fa-chevron-left"></i> Go Back
+              </Link>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12 col-sm-9">
+              <Table hover bordered>
+                <thead>
+                  <tr>
+                    <th>S no.</th>
+                    <th>Item Name</th>
+                    <th>Vendor</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Amount</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.mycart.map((cart, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{cart.itemName}</td>
+                        <td>{cart.vendor}</td>
+                        <td>{cart.quantity}</td>
+                        <td>{cart.price}</td>
+                        <td>{Number(cart.price) * Number(cart.quantity)}</td>
+                        <td className="text-center">
+                          <button
+                            onClick={() => this.deleteItem(index)}
+                            className="btn btn-outline-danger"
+                          >
+                            <i className="fa fa-trash"></i>
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                  <tr>
+                    <td>
+                      <strong>Total Amount</strong>
+                    </td>
+                    <td>
+                      <strong>{this.state.totalAmount} / -</strong>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-6">
+              <h4>
+                Cash On Delivery <i className="fas fa-money-bill"></i>
+              </h4>
+              <input
+                onChange={(e) => this.setAddress(e)}
+                className="w-75 form-control my-3"
+                placeholder="Enter Your Address*"
+              />
+              <button
+                onClick={this.checkOut}
+                className="btn btn-outline-success my-1"
+              >
+                <i className="fas fa-check-double"></i> Check Out{" "}
+              </button>
+            </div>
+            <div className="col-xs-12 col-sm-12 col-md-6">
+              <h4 className="mb-3">
+                Stripe Checkout <i className="fab fa-cc-stripe"></i>
+              </h4>
+              <StripeCheckout
+                stripeKey="pk_test_V086ENyvmzdzrDr3hZB3I5hI006Pgfs0TM"
+                token={this.stripePayment}
+                billingAddress
+                amount={this.state.totalAmount}
+                name="Sweet Panda"
+                email={this.props.auth.user.email}
+              />
+            </div>
+          </div>
+        </div>
+      );
+  }
 }
 
-
 const mapStateToProps = (state) => ({
-    cart : state.cart,
-    auth: state.auth
-})
+  cart: state.cart,
+  auth: state.auth,
+});
 
-export default connect( mapStateToProps,{ fillCart })(MyCart);
+export default connect(mapStateToProps, { fillCart })(MyCart);
--- server.js
+++ server.js
@@ -1,88 +1,89 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const multer = require('multer');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const multer = require("multer");
+const path = require("path");
 
 //Init app
 const app = express();
 
 //Body Parser Middleware
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 app.use(bodyParser.json());
 
 //Multer Middleware
-app.use(multer().single('image'));
+app.use(multer().single("image"));
 
 //DB config
-const dbURL = require('./config/keys').mongoURL;
+const dbURL = require("./config/keys").mongoURL;
 
 //Connect to Mongo DB
 mongoose
-    .connect(dbURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log('MongoDB is connected !'))
-    .catch( error => console.log(error));
-    
+  .connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is connected !"))
+  .catch((error) => console.log(error));
+
 // Index Route
-app.get('/',(req,res)=> {
-    res.send('<h2>Hello from node server</h2>');
-})
+// app.get('/',(req,res)=> {
+//     res.send('<h2>Hello from node server</h2>');
+// })
 
 //User API routes
-const users = require('./routes/api/users');
+const users = require("./routes/api/users");
 
 //Admin API routes
-const admin = require('./routes/admin');
+const admin = require("./routes/admin");
 
 //Bakery API routes
-const bakery = require('./routes/bakers');
+const bakery = require("./routes/bakers");
 
 //Subscriber route
-const subscribe = require('./routes/subscriber');
+const subscribe = require("./routes/subscriber");
 
 //Orders route
-const orders = require('./routes/orders');
+const orders = require("./routes/orders");
 
 //Passport middleware
 app.use(passport.initialize());
 
 //Password config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //User Routes
-app.use('/api/users', users);
+app.use("/api/users", users);
 
 //Bakery Routes
-app.use('/api',bakery);
+app.use("/api", bakery);
 
 //Admin Routes
-app.use('/api',admin);
+app.use("/api", admin);
 
 //Subscriber Route
-app.use('/api',subscribe);
+app.use("/api", subscribe);
 
 //Orders Route
-app.use('/api',orders);
+app.use("/api", orders);
 
 //setting default port for NODE app
 const PORT = process.env.PORT || 5000;
 
 // Server static assets in production
-if(process.env.NODE_ENV === 'production'){
-    
-    //Set static folder
-    app.use(express.static('client/build'));
-    
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    });
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT} !`));
